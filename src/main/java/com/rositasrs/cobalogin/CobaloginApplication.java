841fa07a831154fedcb7ea91223a5893bf8dd105
Rosita120505 <107414498+Rosita120505@users.noreply.github.com>
2022-07-17 19:26:10 +0700

    Initial commit

---
+++ src/main/java/com/rositasrs/cobalogin/CobaloginApplication.java
@@ -0,0 +1,13 @@
+package com.rositasrs.cobalogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rositasrs/cobalogin/CobaloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rositasrs.cobalogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobaloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
