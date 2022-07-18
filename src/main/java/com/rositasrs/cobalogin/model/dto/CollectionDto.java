9139a8516fc80cd231b851abff4bef48d222038e
miniprojectkelompok1 <mini.project.kelompok1@gmail.com>
2022-07-19 06:44:09 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/rositasrs/cobalogin/model/dto/DefaultResponse.java

---
+++ src/main/java/com/rositasrs/cobalogin/model/dto/CollectionDto.java
@@ -0,0 +1,23 @@
+package com.rositasrs.cobalogin.model.dto;
+
+public class CollectionDto {
+
+    private Integer collectionId;
+    private String description;
+
+    public Integer getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(Integer collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
