b1cc61a4cf87eaecc47a46983857696e84e2e665
ridwan18001 <107387036+ridwan18001@users.noreply.github.com>
2022-07-19 08:41:34 +0700

    Delete HomeController.java

--- src/main/java/com/rositasrs/cobalogin/controller/HomeController.java
+++ src/main/java/com/rositasrs/cobalogin/controller/HomeController.java
@@ -1,26 +1,18 @@
 package com.rositasrs.cobalogin.controller;
 
-import antlr.collections.List;
-import com.rositasrs.cobalogin.model.dto.ProductDto;
-import com.rositasrs.cobalogin.model.entity.Product;
 import com.rositasrs.cobalogin.repository.HomeRepository;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping ("/home")
 public class HomeController {
 
-    private HomeRepository homeRepository;
+    private final HomeRepository homeRepository;
 
     public HomeController(HomeRepository homeRepository) {
         this.homeRepository = homeRepository;
     }
 
-    @GetMapping("/terbaru")
-    public List<ProductDto> getListProduct () {
-        List <ProductDto> list = new ArrayList<> ();
-    }
+
 }
