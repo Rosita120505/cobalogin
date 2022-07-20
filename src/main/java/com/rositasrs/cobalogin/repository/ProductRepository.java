6d94e5459465724405b674fdbc238e9ec23add3d
fajrulkamil <fajrul.kamil97@gmail.com>
2022-07-20 16:48:19 +0700

    nambahin fitur sort product by date

--- src/main/java/com/rositasrs/cobalogin/repository/ProductRepository.java
+++ src/main/java/com/rositasrs/cobalogin/repository/ProductRepository.java
@@ -2,11 +2,16 @@ package com.rositasrs.cobalogin.repository;
 
 import com.rositasrs.cobalogin.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
   Optional<Product> findByProductId (Integer productId);
   Optional<Product> findByProductType (String productType);
+
+  @Query(value = "select * from t_product order by product_release_date desc", nativeQuery = true)
+  List<Product> getListNewProduct();
 }
