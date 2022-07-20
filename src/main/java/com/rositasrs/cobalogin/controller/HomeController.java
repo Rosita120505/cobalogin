85847ce9265b92cb6fd69f227a4426f685eebfb0
ridwan18001 <mini.project.kelompok1@gmail.com>
2022-07-20 18:01:10 +0700

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/rositasrs/cobalogin/controller/HomeController.java
+++ src/main/java/com/rositasrs/cobalogin/controller/HomeController.java
@@ -1,69 +1,46 @@
-//package com.rositasrs.cobalogin.controller;
-//
-//import com.rositasrs.cobalogin.model.dto.DefaultResponse;
-//import com.rositasrs.cobalogin.model.dto.ProductDto;
-//import com.rositasrs.cobalogin.model.entity.Product;
-//import com.rositasrs.cobalogin.repository.HomeRepository;
-//import net.bytebuddy.pool.TypePool;
-//import org.hibernate.mapping.Array;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping ("/home")
-//public class HomeController {
-//
-//    private HomeRepository homeRepository;
-//
-//    public HomeController(HomeRepository homeRepository) {
-//        this.homeRepository = homeRepository;
-//    }
-//
-//    @GetMapping ("/latest-product")
-//    public DefaultResponse getByProductReleseDate(){
-//        // kumpulan data sebanyak kita
-//        List<Product> productList = homeRepository.findByProductReleseDate();
-//        DefaultResponse<List<ProductDto>> result = new DefaultResponse();
-//        List<ProductDto> productDtoList = new ArrayList();
-//        for(Product product :  productList){
-//            // masing2 product ini di convert ke dto
-//            productDtoList.add(); // hasil dari convert to dto
-//        }
-//        // kasih kondisinya
-//        result.setMessage("Berikut Adalah 3 Product Terbaru");
-//        result.setStatus(Boolean.TRUE);
-//        result.setData(productDtoList);
-//        return result;
-//    }
-//
-////    @GetMapping("/latest")
-////    //3 produk terbaru
-////    public DefaultResponse getByProductReleseDate (@PathVariable String productReleseDate) {
-////        DefaultResponse df = new DefaultResponse();
-////        Optional <Product> productOptional = homeRepository.findByProductReleseDate();
-////        if (productOptional.isPresent()) {
-////            df.setStatus(Boolean.TRUE);
-////            df.setMessage("Product Relese Date Ditemukan");
-////        } else {
-////            df.setStatus(Boolean.FALSE);
-////            df.setMessage("Product Relese Date Tidak Ditemukan");
-////        }
-////          return df;
-////    }
-//
-////    @GetMapping("/best")
-////    // 3 best product
-////    public List <ProductDto> getListProduct() {
-////
-////        return best;
-////    }
-//
+package com.rositasrs.cobalogin.controller;
+
+import com.rositasrs.cobalogin.model.dto.DefaultResponse;
+import com.rositasrs.cobalogin.model.dto.ProductDto;
+import com.rositasrs.cobalogin.model.entity.Product;
+import com.rositasrs.cobalogin.repository.HomeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping ("/home")
+public class HomeController {
+
+    private HomeRepository homeRepository;
+
+    public HomeController(HomeRepository homeRepository) {
+        this.homeRepository = homeRepository;
+    }
+
+    @GetMapping ("/latest-product")
+    public DefaultResponse getByProductReleseDate(){
+        // menampilkan tiga buah produk terbaru
+        List<Product> productList = homeRepository.findByProductReleseDate();
+        DefaultResponse<List<ProductDto>> result = new DefaultResponse();
+        List<ProductDto> productDtoList = new ArrayList();
+        for(Product product :  productList){
+            // masing-masing product ini di convert ke dto
+            productDtoList.add(convertEntityToDto(product)); // hasil dari convert to dto
+        }
+        // kasih kondisinya
+        result.setMessage("Berikut Adalah 3 Product Terbaru");
+        result.setStatus(Boolean.TRUE);
+        result.setData(productDtoList);
+        return result;
+    }
+
+//3 Product best seller (postponed)
+
 //    public Product convertDtoToEntity(ProductDto dto) {
 //        Product product = new Product();
 //        product.setProductId(dto.getProductId());
@@ -75,16 +52,20 @@
 //
 //        return product;
 //    }
-//
-//    public ProductDto convertEntityToDto (Product entity) {
-//        ProductDto dto = new ProductDto();
-//        dto.setProductId(entity.getProductId());
-//        dto.setProductName(entity.getProductName());
-//        dto.setProductStock(entity.getProductStock());
-//        dto.setPrice(entity.getPrice());
-//        dto.setProductReleaseDate(entity.getProductReleaseDate());
-//        dto.setProductStockFinal(entity.getProductStockFinal());
-//
-//        return dto;
-//    }
-//}
+
+    public ProductDto convertEntityToDto (Product entity) {
+        ProductDto dto = new ProductDto();
+        dto.setProductId(entity.getProductId());
+        dto.setProductName(entity.getProductName());
+        dto.setColorId(entity.getColorId());
+        dto.setProductStock(entity.getProductStock());
+        dto.setPrice(entity.getPrice());
+        dto.setProductDescription(entity.getProductDescription());
+        dto.setProductWeight(entity.getProductWeight());
+        dto.setProductReleaseDate(entity.getProductReleaseDate());
+        dto.setProductStockFinal(entity.getProductStockFinal());
+        dto.setProductType(entity.getProductType());
+
+        return dto;
+    }
+}
--- src/main/java/com/rositasrs/cobalogin/repository/HomeRepository.java
+++ src/main/java/com/rositasrs/cobalogin/repository/HomeRepository.java
@@ -1,37 +1,19 @@
-//<<<<<<< Updated upstream
-//package com.rositasrs.cobalogin.repository;
-//
-//import com.rositasrs.cobalogin.model.entity.Product;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//
-//import java.util.Date;
-//import java.util.List;
-//import java.util.Optional;
-//
-//public interface HomeRepository extends JpaRepository <Product, Integer> {
-//
+package com.rositasrs.cobalogin.repository;
+
+import com.rositasrs.cobalogin.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface HomeRepository extends JpaRepository <Product, Integer> {
+
 //    Optional<Product> findByProductStockProductStockFinal (Integer ProductStock, Integer ProductStockFinal);
 //
 //    Optional<Product> findByProductReleseDate(String ProductReleseDate);
-//
-//    @Query(value = "select * from table order by tanggal limit 3", nativeQuery = true)
-//    List<Product> findNewProduct();
-//}
-//=======
-////package com.rositasrs.cobalogin.repository;
-////
-////import com.rositasrs.cobalogin.model.entity.Product;
-////import org.springframework.data.jpa.repository.JpaRepository;
-////
-////import java.util.Date;
-////import java.util.Optional;
-////
-////public interface HomeRepository extends JpaRepository <Product, Integer> {
-////
-////    Optional<Product> findByProductId (Integer productId);
-////    Optional<Product> findByProductStockProductStockFinal (Integer ProductStock, Integer ProductStockFinal);
-////
-////    Optional<Product> findByProductReleseDate(Date ProductRelesedate);
-////}
-//>>>>>>> Stashed changes
+
+//    @Query(value = "select * from table order by tanggal limit ?", nativeQuery = true)
+    @Query(value = "select * from Product order by ProductReleseDate limit 3", nativeQuery = true)
+    List<Product> findByProductReleseDate();
+}
+
