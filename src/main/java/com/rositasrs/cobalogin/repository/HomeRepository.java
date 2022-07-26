cacdad2a75342d87c6733f02d73a9d387398dace
ridwan18001 <mini.project.kelompok1@gmail.com>
2022-07-26 12:08:31 +0700

    update home, add expediiton

--- src/main/java/com/rositasrs/cobalogin/repository/HomeRepository.java
+++ src/main/java/com/rositasrs/cobalogin/repository/HomeRepository.java
@@ -1,5 +1,6 @@
 package com.rositasrs.cobalogin.repository;
 
+import com.rositasrs.cobalogin.model.dto.ProductDto;
 import com.rositasrs.cobalogin.model.dto.projection.BestSellerDto;
 import com.rositasrs.cobalogin.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,12 +15,12 @@ public interface HomeRepository extends JpaRepository <Product, Integer> {
 //    Optional<Product> findByProductReleseDate(String ProductReleseDate);
 
 //    @Query(value = "select * from table order by tanggal limit ?", nativeQuery = true)
-@Query(value = "select * from t_product order by product_release_date desc limit 3", nativeQuery = true)
-    List<Product> getProductReleseDate();
+@Query(value = "select * from t_product order by product_release_date desc limit 3;", nativeQuery = true)
+    List<Product> getProductReleaseDate();
 
-    @Query(value = "select product_id, product_name, color_id, product_stock - product_stock_final as total_selling\n" +
-            "from t_product\n" + "order by total_selling desc\n" +
-            "limit 3", nativeQuery = true)
+    @Query(value = "select product_id productId, product_name productName, color_id colorId, product_stock - product_stock_final as totalSelling\n" +
+            "from t_product\n" +
+            "order by totalSelling desc limit 3", nativeQuery = true)
     List<BestSellerDto> getBestSeller();
 
 }
